1d3366e8b813d9d0688306edbd8d422d936a165b
ahnaufal14 <ahmadnaufalrifaah@gmail.com>
2026-03-12 11:15:24 +0700

    Add files via upload

---
+++ latihan 1.1. tipe data dan variabel.py
@@ -0,0 +1,16 @@
+#tipe data string
+nama_kota = "kediri"
+
+#tipe data integer
+jumlah_penduduk = 10000
+
+#tipe data float
+suhu_kota = 30.5
+
+#tipedatabool
+apakah_panas_suhunya = True
+
+print("ini adalah kota", nama_kota) #mencetak hasil tipe data string
+print("jumlah nya penduduknya adalah", jumlah_penduduk) #mencetak hasil tipe data integer
+print("suhu di kota tersebut adalah", suhu_kota) #mencetak hasil tipe data float
+print("apakah panas suhunya?", apakah_panas_suhunya) #mencetak hasil tipe data bool
